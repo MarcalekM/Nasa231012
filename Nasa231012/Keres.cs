e0b4562104e99a64e5b35b1fc8f5e75e51e567af
Máté Marcalek <matemarcalek@gmail.com>
2023-10-12 10:26:34 +0200

    Add project files.

---
+++ Nasa231012/Keres.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nasa231012
+{
+    class Keres
+    {
+        public string Cim { get;  set; }
+        public String DatumIdo { get;  set; }
+        public string Get { get; set; }
+        public string HttpKod { get; set; }
+        public string Meret { get; set; }
+
+        public Keres(string s)
+        {
+            var v = s.Split('*');
+            Cim = v[0];
+            DatumIdo = v[1];
+           Get = v[2];
+            var a = v[3].Split(' ');
+            HttpKod = a[0];
+            Meret = a[1];
+        }
+    }
+}
---
+++ Nasa231012/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.Encodings;
+using System.Text;
+
+namespace Nasa231012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Keres> keresek = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\NASAlog.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) keresek.Add(new(sr.ReadLine()));
+            Console.WriteLine($"{keresek.Count}") ;
+
+            Console.ReadLine();
+        }
+    }
+}
